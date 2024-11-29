4548e08aecf8809618af1639cac498ba8bc81966
Gradin Gheti <gradin_gheti@netex.ro>
2024-11-29 14:39:29 +0200

    improve form security add recaptcha

--- app/(main)/contact/page.tsx
+++ app/(main)/contact/page.tsx
@@ -253,7 +253,7 @@ export default function Contact() {
         <button
           className='bg-gradient-to-br relative group/btn from-zinc-900  to-neutral-600 block dark:bg-zinc-800 w-full text-white rounded-md h-10 font-medium shadow-[0px_1px_0px_0px_#ffffff40_inset,0px_-1px_0px_0px_#ffffff40_inset] dark:shadow-[0px_1px_0px_0px_var(--zinc-800)_inset,0px_-1px_0px_0px_var(--zinc-800)_inset]'
           type='submit'
-          disabled={isSubmitting || isVerified}
+          disabled={isSubmitting || !isVerified}
         >
           Contact us &rarr;
           <BottomGradient />
